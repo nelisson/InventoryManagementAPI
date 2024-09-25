25951611712551a20730712ee61afc7e8c0bb532
Nélisson Cavalheiro <nelisson.ad@gmail.com>
2024-09-24 22:20:25 -0300

    Add Product class with validation attributes

---
+++ InventoryManagementAPI/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventoryManagementAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int Stock { get; set; }
+
+        [Timestamp]
+        public required byte[] RowVersion { get; set; }
+    }
+}
