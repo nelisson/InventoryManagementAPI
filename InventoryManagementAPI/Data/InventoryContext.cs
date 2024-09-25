d05296d25fb5d623fde9727b40b055110f34289e
Nélisson Cavalheiro <nelisson.ad@gmail.com>
2024-09-24 22:24:30 -0300

    Add Model and Inventory context

---
+++ InventoryManagementAPI/Data/InventoryContext.cs
@@ -0,0 +1,10 @@
+﻿using InventoryManagementAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagementAPI.Data
+{
+    public class InventoryContext(DbContextOptions<InventoryContext> options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
